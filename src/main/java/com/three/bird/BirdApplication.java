eda56587f6f3a3cc717475512c9b3458d0b13599
fengpeiyao <crazyfengpeiyao@163.com>
2019-06-13 15:59:01 +0800

    first commit

---
+++ src/main/java/com/three/bird/BirdApplication.java
@@ -0,0 +1,13 @@
+package com.three.bird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirdApplication.class, args);
+    }
+
+}
